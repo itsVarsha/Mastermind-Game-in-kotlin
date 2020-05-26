6c7ef6214e99139795b525e4ba5e1477d5e1f9ea
Varsha Sharma <2520varsha@gmail.com>
2020-05-27 04:18:23 +0530

    Update evaluateGuess.kt

--- Mastermind(Solution)/src{solution}/evaluateGuess.kt
+++ Mastermind(Solution)/src{solution}/evaluateGuess.kt
@@ -6,34 +6,34 @@ data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(Secret: String, Guess: String): Evaluation {
    println("$Secret.. $Guess")
-var secret=Secret;
-    var guess=Guess;
-    var rightPosition = 0;
+var secret=Secret
+    var guess=Guess
+    var rightPosition = 0
     var rightCh: String  =""
-    var wrongCh: String=""
-    var wrongPosition = 0;
+    var wrongCh: String  =""
+    var wrongPosition = 0
 
-  var i:Int = 0;
+  var i:Int = 0
 
        var len=secret.length
-while (i<len) {//guess
+while (i<len) {                                 //guess
 
     if(guess[i]==secret[i])
-    {//secret=AABC, guess=ADFA   R: 1, w: 1
-        rightPosition++     // R: 1, W: 2
+    {                                        //secret=AABC, guess=ADFA   R: 1, w: 1
+        rightPosition++       
         rightCh+= guess[i]
         secret = secret.removeRange(i,i+1)
         guess=guess.removeRange(i,i+1)
-        len=secret.length;
-        ; continue
+        len=secret.length
+        continue
     }
-    i++;
+    i++
 }
-    i=0;
+    i=0
 
-    while(i<len)//guess
-    {// secret=ABC, guess=DFA
-    var ii:Int=0; //secret
+    while(i<len)                                   //guess
+    {                                              // secret=ABC, guess=DFA
+    var ii:Int=0;                                     //secret
         while(ii<len) {
             if (guess[i]!='*' && guess[i] == secret[ii]) {
 
